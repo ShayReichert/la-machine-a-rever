2f1594cf0fa30a694d09b273e242a829d885c18f
Shay Reichert <shay.reichert@outlook.fr>
2022-01-12 16:26:24 +0100

    init

--- src/index.js
+++ src/index.js
@@ -1,14 +1,62 @@
 import './main.css';
-import styles from './index.module.css';
 
-const h1 = document.querySelector('h1');
-h1.classList.add(styles.h1);
+/* Get the documentElement (<html>) to display the page in fullscreen */
+const elem = document.documentElement;
 
-async function print() {
-  // Here we are using dynamic import
-  const { greet } = await import('./greet');
-  const response = await greet('John Doe');
-  console.log(response);
+// Fullscreen au double clic
+document.addEventListener("dblclick", function (e) {
+  openFullscreen();
+});
+
+// Quitter fullscreen touche 'echap'
+document.body.addEventListener("keydown", function (e) {
+  if (e.key == "Escape") {
+    closeFullscreen();
+  }
+});
+
+// Start / Stop clignotement
+document.body.addEventListener("keydown", function (e) {
+  if (e.which == 32) {
+    const white = document.querySelector("#white");
+
+    if (white.classList.contains("active")) {
+      white.classList.remove("active");
+    } else {
+      white.classList.add("active");
+    }
+  }
+});
+
+
+/* --- FUNCTIONS --- */
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE11 */
+    elem.msRequestFullscreen();
+  }
 }
 
-print();
+/* Close fullscreen */
+function closeFullscreen() {
+  if (!window.screenTop && !window.screenY) {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      /* Safari */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      /* IE11 */
+      document.msExitFullscreen();
+    }
+  }
+}
+
+// TODO : au clic sur touche 1, 2 ou 3, changer la vitesse de clignotement.
